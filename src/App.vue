47c21bd8bcb785414ccd5587a53e921871dc6592
khannasir <nasirkhan1934@gmail.com>
2024-08-22 20:51:03 +0530

    change nav menu color

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <div class="max-w-7xl mx-auto">
       <nav class="m-2 p-2">
-        <RouterLink to="/" class="px-4 py-2 mr-4 text-slate-50 bg-blue rounded">Home</RouterLink>
-        <RouterLink to="/user" class="px-4 py-2 mr-4 text-slate-50 bg-blue rounded"
+        <RouterLink to="/" class="px-4 py-2 mr-4 text-slate-50 bg-black rounded">Home</RouterLink>
+        <RouterLink to="/user" class="px-4 py-2 mr-4 text-slate-50 bg-black rounded"
           >Users</RouterLink
         >
       </nav>
